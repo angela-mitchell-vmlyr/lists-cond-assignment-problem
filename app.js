9cd123e9db08c5dc26369eaf5ed94285380a6be2
Angela Mitchell <angela.mitchell@vmlyr.com>
2021-10-07 23:27:37 -0500

    move logic into JavaScript code

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = Vue.createApp({
             showList: true,
         }
     },
+    computed: {
+        buttonCaption() {
+            // Best practice: move complex logic out of HTML code into JavaScript code
+            // This is a scenario where using a computed property makes a lot of sense
+            return this.showList ? 'Hide List' : 'Show List';
+        }
+    },
     methods: {
         addTask() {
             this.tasks.push(this.userInput);
