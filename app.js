db10b2cba56565e1ef0b3c06e6e5fd89492b79b4
Angela Mitchell <angela.mitchell@vmlyr.com>
2021-10-07 23:06:03 -0500

    render a list of task using Vue directives

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            
+            tasks: [],
+            userInput: '',
+            showList: true,
+        }
+    },
+    methods: {
+        addTask() {
+            this.tasks.push(this.userInput);
+        },
+        toggleList() {
+            if (this.tasks.length > 0) {
+                this.showList = !this.showList;
+            }
         }
     }
 });
